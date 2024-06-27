34a1b4d7f14022bab3139c85098dedd7d62d7951
Eng-Liban <LibanAbdulahi0008@gmail.com>
2024-06-27 23:37:05 +0300

    file

--- backend/Server.js
+++ backend/Server.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
 let mongodb = mongoose.connect(
-  "mongodb=mongodb+srv://libanabdulahi2024:zNRiQgNiIgAJ8oX5@cluster0.aeteeum.mongodb.net/Mern-Chat-App?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://libanabdulahi2024:zNRiQgNiIgAJ8oX5@cluster0.aeteeum.mongodb.net/Mern-Chat-App?retryWrites=true&w=majority&appName=Cluster0"
 );
 Server.listen(process.env.PORT || 3000, mongodb, () =>
   console.log(process.env.SERVER || "Server running on Port 3000")
