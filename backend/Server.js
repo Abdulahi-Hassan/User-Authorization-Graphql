ef31cfa70c296106fff70d5c956eaa59b7651025
Eng-Liban <LibanAbdulahi0008@gmail.com>
2024-06-27 23:24:47 +0300

    file

--- backend/Server.js
+++ backend/Server.js
@@ -2,5 +2,10 @@ const express = require("express");
 const Server = require("./config/Server");
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
-let mongodb = mongoose.connect("mongodb://localhost:27017/User-Authorization");
-Server.listen(3000, mongodb, () => console.log(process.env.SERVER));
+
+let mongodb = mongoose.connect(
+  "mongodb=mongodb+srv://libanabdulahi2024:zNRiQgNiIgAJ8oX5@cluster0.aeteeum.mongodb.net/Mern-Chat-App?retryWrites=true&w=majority&appName=Cluster0"
+);
+Server.listen(process.env.PORT || 3000, mongodb, () =>
+  console.log(process.env.SERVER || "Server running on Port 3000")
+);
